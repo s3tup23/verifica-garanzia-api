d5af0f0f91caf4c1916031edfd2afa0cda3a247d
s3tup23 <165216439+s3tup23@users.noreply.github.com>
2025-06-23 22:57:55 +0200

    Update ordini.js

--- api/ordini.js
+++ api/ordini.js
@@ -2,6 +2,11 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+res.setHeader('Vary', 'Origin');
+
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end(); // Preflight CORS OK
